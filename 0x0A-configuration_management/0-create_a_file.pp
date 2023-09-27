5830b2159b249251bf3265100045a810327fa9e2
washucode <estherw8525@gmail.com>
2023-09-27 19:56:01 +0300

    task 0 corrections

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # This manifest creates a file in /tmp
 
-file{'school':
-    ensure  => file,
-    path    => '/tmp/school',
+file{'/tmp/school':
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet'
